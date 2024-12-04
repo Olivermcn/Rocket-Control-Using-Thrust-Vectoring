a6977412de4ca25f3c269671f358c5091f225f1e
Olivermcn <101463133+Olivermcn@users.noreply.github.com>
2024-12-04 14:16:09 +0000

    Initialisation

---
+++ Parameters.m
@@ -0,0 +1,7 @@
+% Parameters for the model
+mass_ini = 1000;               % Mass of the object in kg
+Ixx_ini = 5000; % Initial moment of inertia Ixx in kg*m^2
+Izz_ini = 5000; % Initial moment of inertia Ixx in kg*m^2
+gravitational_acceleration = 9.81; % Gravitational acceleration in m/s^2
+
+
